63d352ecbab22636c1dd076ae4a09e20c5a11113
jncherry <jason.cherry@ymail.com>
2015-05-06 22:24:05 -0700

    First commit, basic GUI framework one passing unit test and add functionality for "Enter Transaction Button"

---
+++ AcctMgr/MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AcctMgr
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCancelTransaction_Click(object sender, RoutedEventArgs e)
+        {
+            ClearAll();
+        }
+
+        private void btnEnterTrans_Click(object sender, RoutedEventArgs e)
+        {
+            double amt, cred, deb, trans;           
+
+            try
+            {
+
+                if (cboTransType.SelectedIndex == 1)
+                {
+                
+                    if (Double.TryParse(txtTransAmt.Text, out amt))
+                    {
+                        Double.TryParse(txtTotalCredit.Text, out cred);
+                        txtAcctBal.Text = AddAmt(amt).ToString();
+                        cred += amt;
+                        txtTotalCredit.Text = cred.ToString();
+                        ClearAll();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Error! Transaction amount must be numeric!", "Invalid Input!", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                    
+                              
+                }
+                else if (cboTransType.SelectedIndex == 0)
+                {
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            } 
+        }
+
+        public double AddAmt(double _amt)
+        {
+            double transAmt, newBal = 0;
+
+            try
+            {
+                Double.TryParse(txtAcctBal.Text, out transAmt);                
+                newBal = _amt + transAmt;                  
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Error: " + e.Message);
+            }
+            
+            return newBal; 
+        }
+
+        public void ClearAll()
+        {
+            cboAccounts.SelectedIndex = -1;
+            txtTransAmt.Text = "";
+            cboTransType.SelectedIndex = -1;
+            txtDesc.Text = "";
+        }
+    }
+}
---
+++ UnitTest_AcctMgr/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest_AcctMgr
+{
+    [TestClass]
+    public class TestAddFunciton_ValidAmount
+    {
+        [TestMethod]
+        public void TestAdd()
+        {
+            double expectedAmount = 175.00;
+            double creditAmt = 50.00;
+            AcctMgr.MainWindow window = new AcctMgr.MainWindow();
+            double value = window.AddAmt(creditAmt);
+            Assert.AreEqual(value, expectedAmount);
+        }
+    }
+}
